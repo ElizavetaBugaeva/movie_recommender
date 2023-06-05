380f11f6b565d0becd6805f373a55178dda5f7a5
Elizaveta_Bugaeva <elizavetabugaeva96@gmail.com>
2023-06-05 12:03:01 +0200

    fix files

--- utils.py
+++ utils.py
@@ -12,8 +12,8 @@ MOVIES = ['John Wick',
 
 
 
-with open('/Users/elizavetabugaeva/Documents/Spiced/weekly_milestones/week_10/week_10/NMF/nmf_model1.pkl', 'rb') as file:
+with open('nmf_model1.pkl', 'rb') as file:
     nmf_model = pickle.load(file)
 
-with open('/Users/elizavetabugaeva/Documents/Spiced/weekly_milestones/week_10/cosin_recommender.pkl', 'rb') as file:
+with open('cosin_recommender.pkl', 'rb') as file:
     cos_sim_model = pickle.load(file)
